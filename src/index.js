d13ad873c735131ec4a2ae9925116c7e3de7d536
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2024-02-10 19:38:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "cloudflare-docker-proxy.dhanushreddy29.workers.dev/": "https://registry-1.docker.io",
+  "cloudflare-docker-proxy.dhanushreddy29.workers.dev/": "https://index.docker.io/v2",
 };
 
 function routeByHosts(host) {
